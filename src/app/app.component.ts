cf70528abe31b188c5c3ac8dff3517e838f90229
Noa <nonistein@gmail.com>
2017-11-20 17:21:14 +0200

    add toggle navbar button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  something: boolean;
+
+  displaySidebar() {
+    console.log('Hi');
+    (this.something === true) ? this.something = false : this.something = true;
+  };
 }
+
